08325a1b75f0896574088d8f79db8888bb2f2f98
Lucas Augusto <lucas@gmail.com>
2024-08-29 18:02:34 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
+++ src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
@@ -2,6 +2,7 @@ package com.educandoweb.course.entities.pk;
 
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.Product;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -30,6 +31,7 @@ public class OrderItemPK implements Serializable {
         this.product = product;
     }
 
+    @JsonIgnore
     public Order getOrder() {
         return order;
     }
